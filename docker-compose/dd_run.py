340308c6cf27811b5b437ceced5da6ff58cf0190
witekdev <witekdev@gmail.com>
2018-07-22 18:19:32 -0600

    add command line parameter options for specifying browser and docker_machine_ip, add ms edge launching support.

--- docker-compose/dd_run.py
+++ docker-compose/dd_run.py
@@ -2,35 +2,50 @@
 
 # ======================================================================================================
 # Script settings:
-browser = 'firefox' # Possible options are 'firefox', 'iexplore', 'chrome', 'opera'
-					# modify the script on your own below, if other browser support is needed (line 67)
+browser = 'firefox' # Possible options are 'firefox', 'iexplore', 'chrome', 'opera' 'microsoft-edge'
+					# modify the script on your own below, if other browser support is needed (line 82)
 log_file = 'log_dallinger.txt' # Name of output log file to read from
 new_window = True # Open new browser windows (Set to False to reuse existing browser windows)
-docker_machine_ip = "192.168.99.100" # docker-machine address
 dallinger_startup_delay = 2 # delay in seconds to allow dallinger to complete its startup processes
 override_port = True # This will override the port of the experiment to port 5000
 # ======================================================================================================
 
+import getopt
 import os
 import re
 import subprocess
 import sys
 import time
 
-# TODO?
-# add parameters machine-ip
-# add browser parameter via cmd
+# Parse user defined parameters for browser and docker_machine ip (if present)
+docker_machine_ip = ""
+argv = sys.argv[1:]
+try:
+	opts, args = getopt.getopt(argv,"hb:i:",["browser=","machine_ip="])
+except getopt.GetoptError:
+	print('dd_run.py -b <browser> -i <machine_ip_address>')
+	sys.exit(2)
+for opt, arg in opts:
+	if opt == '-h':
+		print('dd_run.py -b <browser> -i <machine_ip_address>')
+		sys.exit()
+	elif opt in ("-b", "--browser"):
+		browser = arg
+	elif opt in ("-i", "--machine_ip"):
+		docker_machine_ip = arg
 
+# Python 2 and 3 use different urlparse methods
 if sys.version_info[0] == 2:
 	from urlparse import urlparse as urlparse
 elif sys.version_info[0] == 3:
 	from urllib import parse as urlparse
 
 try:
-	# the IP to use will be read directly from docker ideally
-	command = "docker-machine ip"
-	docker_machine_ip = subprocess.check_output(['bash','-c', command])
-	docker_machine_ip = docker_machine_ip.strip('\n')
+	# the IP to use will be read directly from docker ideally (unless user overriden)
+	if docker_machine_ip == "":
+		command = "docker-machine ip"
+		docker_machine_ip = subprocess.check_output(['bash','-c', command])
+		docker_machine_ip = docker_machine_ip.strip('\n')
 except:
 	if docker_machine_ip == "":
 		docker_machine_ip = "0.0.0.0" # backup default
@@ -64,8 +79,8 @@ print('')
 print(' Use CONTRL-C to stop this script ');
 print('')
 
-if browser not in ['firefox', 'iexplore', 'chrome', 'opera']:
-	browser = 'iexplore' # XXX check for edge in win10?
+if browser not in ['firefox', 'iexplore', 'chrome', 'opera', 'microsoft-edge']:
+	browser = 'iexplore'
 	
 urls = []
 parsed_urls = []
@@ -79,7 +94,6 @@ while not experiment_complete:
 		command = "docker-compose logs dallinger |& tee " + log_file
 		output = subprocess.check_output(['bash','-c', command])
 		print("Reading Dallinger output log..")
-#		time.sleep(1)
 
 		f = open(os.path.join(__location__, log_file), 'r');
 		lines = f.readlines()
@@ -108,7 +122,13 @@ while not experiment_complete:
 		for url in parsed_urls:
 			if url not in displayed_urls:
 				print("Displaying: " + url)
-				if new_window:
+				if browser == 'microsoft-edge': # uses different syntax (does not support new window openings)
+					command = 'start ' + browser + ':\"' + url + '\"'
+				elif browser == 'iexplore':
+				    # ie opens up new windows by default (win7) and does not recognize new_window parameter
+					command = 'start ' + browser + ' \"' + url + '\"'
+				# all other browsers:
+				elif new_window:
 					command = 'start ' + browser + ' -new-window \"' + url + '\"'
 				else:
 					command = 'start ' + browser + ' \"' + url + '\"'
